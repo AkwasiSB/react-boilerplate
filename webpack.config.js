ec59a82484b6b4faf1ba2a29581413a8c74a2479
AkwasiSB <akwasisefaboakye@yahoo.com>
2019-03-30 01:41:18 +0000

    made small changes to boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -5,78 +5,85 @@ const StyleLintPlugin = require('stylelint-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = env => {
-  return {
-    mode: env.development ? 'development' : 'production',
-    entry: './src/index.js',
-    output: {
-      filename: 'app.bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-      rules: [
-        {
-          enforce: 'pre',
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'eslint-loader'
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-              loader: 'babel-loader',
-              options: {
-                  presets: ['@babel/preset-env', '@babel/preset-react']
-              }
-          }
-        },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          exclude: /node_modules/,
-          use: [
-            env.development ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                localIdentName: '[name]__[local]-[hash:base64:6]',
-                importLoaders: 2
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                ident: 'postcss',
-                plugins: () => [
-                  require('postcss-preset-env')({
-                    browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie <= 8']
-                  })
-                ]
-              }
-            },
-            'sass-loader'
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({ template: './public/index.html' }),
-      new StyleLintPlugin({
-        configFile: './.stylelintrc',
-        syntax: 'scss',
-        ignorePath: './.stylelintignore'
-      }),
-      new MiniCssExtractPlugin({
-        filename: '[name].[hash].css',
-        chunkFilename: '[id].[chunkhash].css'
-      })
-    ],
-    devtool: env.development ? 'eval-source-map' : 'source-map',
-    devServer: {
-      open: true,
-      port: 4000,
-      stats: 'minimal',
-    }
-  }
+	return {
+		mode: env.development ? 'development' : 'production',
+		entry: './src/index.js',
+		output: {
+			filename: 'app.bundle.js',
+			path: path.resolve(__dirname, 'dist')
+		},
+		module: {
+			rules: [
+				{
+					enforce: 'pre',
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'eslint-loader'
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+							loader: 'babel-loader',
+							options: {
+									presets: ['@babel/preset-env', '@babel/preset-react']
+							}
+					}
+				},
+				{
+					test: /\.(sa|sc|c)ss$/,
+					exclude: /node_modules/,
+					use: [
+						env.development ? 'style-loader' : MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true,
+								localIdentName: '[name]__[local]-[hash:base64:6]',
+								importLoaders: 2,
+								camelCase: true
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								ident: 'postcss',
+								plugins: () => [
+									require('postcss-preset-env')({
+										browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie <= 8']
+									})
+								]
+							}
+						},
+						'sass-loader'
+					]
+				},
+				{
+					test: /\.(png|jpg|jpeg|gif)$/,
+					exclude: /node_modules/,
+					loader: 'file-loader'
+				}
+			]
+		},
+		plugins: [
+			new CleanWebpackPlugin(['dist']),
+			new HtmlWebpackPlugin({ template: './public/index.html' }),
+			new StyleLintPlugin({
+				configFile: './.stylelintrc',
+				syntax: 'scss',
+				ignorePath: './.stylelintignore'
+			}),
+			new MiniCssExtractPlugin({
+				filename: '[name].[hash].css',
+				chunkFilename: '[id].[chunkhash].css'
+			})
+		],
+		devtool: env.development ? 'eval-source-map' : 'source-map',
+		devServer: {
+			historyApiFallback: true,
+			open: true,
+			port: 4000,
+			stats: 'minimal',
+		}
+	}
 }
